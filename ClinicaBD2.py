f1d1735d46e9ef91780d9836dd7e9b2565aaea58
Gabriel Silva <92138630+MajorDino@users.noreply.github.com>
2022-12-10 17:32:48 -0300

    Update ClinicaBD2.py

--- ClinicaBD2.py
+++ ClinicaBD2.py
@@ -1,4 +1,10 @@
 from datetime import date
+import psycopg
+
+connection = psycopg.connect(dbname="projectBD2", user="", password="postgres")
+#alterar dbname (que é o nome do BD no pgAdmin 3), user e password de acordo com o database que for criar no pgAdmin 3 pra rodar esse código
+#colocar os parametros port="", host="" se necessário, caso não utilize os padrões (que são localhost)
+cursor = connection.cursor()
 
 class Endereco():
     def __init__(self,uf: str, num: int, rua: str, cidade: str, bairro: str) -> None:
@@ -17,19 +23,63 @@ class Paciente():
         self.dddPaciente = dddPaciente
         self.enderecoPaciente = enderecoPaciente
         #Insert no BD
+        try:
+            postgres_insert_query = """ INSERT INTO paciente (nomePaciente, cpfPaciente, dataNascPaciente, telefonePaciente, dddPaciente, enderecoPaciente) VALUES (%s,%s,%s,%s,%s,%s)"""
+            record_to_insert = (self.nomePaciente, self.cpfPaciente, self.dataNascPaciente, self.telefonePaciente, self.dddPaciente, self.enderecoPaciente)
+            cursor.execute(postgres_insert_query, record_to_insert) #realiza o insert. Não sei o que esse record_to_insert retorna e nem porq o cara que eu vi fez assim
+            #eu preferia ter feito um comando de 1 linha só, mas ok
+            
+            connection.commit()
+            count = cursor.rowcount
+            print(count, "Insercao no banco de dados realizada com sucesso!")
+        except:
+            print("Falha ao realizas insercao no banco de dados!", error)
         
     def updateNomePaciente(self, nome):
-        self.nomePaciente = nome
         #Update no BD
+        sql_update_query = """Update paciente set nomePaciente = %s where nomePaciente = %s"""
+        cursor.execute(sql_update_query, (nome, self.nomePaciente))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Nome do paciente atualizado com sucesso! ")
+        
+        self.nomePaciente = nome #resolvi alterar o self.nomePaciente somente no final pq a linha 41 precisa de 2 parametros: o nome novo e o antigo
+        #o antigo serve pra ele procurar no banco qual dos pacientes vai sofrer o update e receber o nome novo no lugar
+        #se preferir, em vez de usar nomePaciente pra buscar o paciente que deve ser atualizado, pode-se usar cpfPaciente que parece mais certo!
+        #usei a mesma lógica desse update aqui nos de baixo...
     def updateDddTelefonePaciente(self, ddd, telefone ):
+        #Update no BD
+        sql_update_query = """Update paciente set dddPaciente = %s where dddPaciente = %s"""
+        cursor.execute(sql_update_query, (ddd, self.dddPaciente))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "DDD do paciente atualizado com sucesso! ")
+        sql_update_query = """Update paciente set telefonePaciente = %s where telefonePaciente = %s"""
+        cursor.execute(sql_update_query, (telefone, self.telefonePaciente))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Telefone do paciente atualizado com sucesso! ")
+        
         self.dddPaciente = ddd
         self.telefonePaciente = telefone
-        #Update no BD
     def addPlano(self,codPlano: int):
-        self.codPlano = codPlano
         #Update no BD
+        #FIQUE EM DUVIDA DE COMO FAZER!!!!!
+        #tem que se atualizar a variavel fk_codPlano da tabela paciente ou a variável pk_codPlano da tabela plano?
+        #acho que seguindo os exemplos acima, vc desenrola como fazer...
+        
+        self.codPlano = codPlano
     def deletePaciente(self):
         #Delete no BD
+        try:
+            sql_delete_query = """Delete from paciente where nomePaciente = %s"""
+            cursor.execute(sql_delete_query, (self.nomePaciente,))
+            connection.commit()
+            count = cursor.rowcount
+            print(count, "Paciente deletado com sucesso (morreu)...")
+
+        except (Exception, psycopg2.Error) as error:
+            print("Erro ao deletar paciente (tá vivo).", error)
         del self
            
 
@@ -77,5 +127,10 @@ class Agendamento():
         self.dataAgendamento = date.today()
         self.codMedico = codMedico
         #self.codConsulta =  ( puxar do banco de dados a ultima consulta gerada e incrementar )
+        cur.execute("select * from agendamento")
+        resultado = cursor.fetchall() #coloca os resultados do último execute em um resultset
+        for dados in resultado:
+            print(dados) #percorre o resultset e a tabela linha por linha com seus respectivos dados
+            #basta pegar a última linha de acordo com o comprimento de 'resultado' e formatar o seu print pra ficar bonitinho
         #Insert no BD na tabela Consulta
         
